b8ea03eef9669a4f5d861b199e6760038f30baeb
Scott Sproule <scott.sproule@ficonab.com>
2024-08-23 22:41:28 -0400

    added some tests

--- test/vsoi_web/controllers/page_controller_test.exs
+++ test/vsoi_web/controllers/page_controller_test.exs
@@ -3,6 +3,21 @@ defmodule VsoiWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Verde Smart Organiku International"
+  end
+
+  test "GET /vision", %{conn: conn} do
+    conn = get(conn, ~p"/vision")
+    assert html_response(conn, 200) =~ "highest quality organic food"
+  end
+
+  test "GET /team", %{conn: conn} do
+    conn = get(conn, ~p"/team")
+    assert html_response(conn, 200) =~ "Eka Mardiarti"
+  end
+
+  test "GET /products", %{conn: conn} do
+    conn = get(conn, ~p"/products")
+    assert html_response(conn, 200) =~ "Onions"
   end
 end
